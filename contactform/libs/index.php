32e29b9dee5d62c0097493c75a4fd87ac435fd13
BuildTools <unconfigured@null.spigotmc.org>
2016-11-27 03:38:21 -0800

    Footer fixes and php contact implementation
    Adjusted the footer on all pages to show up correctly on mobile.
    
    Implemented PHP to make the contact page work.

---
+++ contactform/libs/index.php
@@ -0,0 +1,18 @@
+<?php
+if ($_POST['submit']) {
+  if (mail ($to, $subject, $body, $from)) {
+    echo '<p>Your Message has been sent.</p>';
+  } else {
+    echo '<p>Something went wrong. :(</p>';
+  }
+
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $message = $_POST['message'];
+      $from = 'From: buttstuff';
+      $to = 'lucastoltman@gmail.com';
+      $subject = 'Contact Form Response';
+
+      $body = "From: $name\n Email: $email\n Message:\n $message";
+}
+?>
