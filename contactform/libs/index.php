75daa4da507fb2ac10375b7e8c7e61fd8d3aff2a
BuildTools <unconfigured@null.spigotmc.org>
2016-11-27 04:05:44 -0800

    Revert "Added a trailing slash"
    This reverts commit 427fb989fdc439d70f8e439bbd19e53a4ab2d686.

--- contactform/libs/index.php
+++ contactform/libs/index.php
@@ -1,17 +1,17 @@
 <?php
-if ($_POST["submit"]) {
+if ($_POST['submit']) {
   if (mail ($to, $subject, $body, $from)) {
     echo '<p>Your Message has been sent.</p>';
   } else {
     echo '<p>Something went wrong. :(</p>';
   }
 
-      $name = $_POST["name"];
-      $email = $_POST["email"];
-      $message = $_POST["message"];
-      $from = "From: buttstuff";
-      $to = "lucastoltman@gmail.com";
-      $subject = "Contact Form Response";
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $message = $_POST['message'];
+      $from = 'From: buttstuff';
+      $to = 'lucastoltman@gmail.com';
+      $subject = 'Contact Form Response';
 
       $body = "From: $name\n Email: $email\n Message:\n $message";
 }
