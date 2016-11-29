9e8ec26092c9ab0997d0724bc0e52580c946e31f
BuildTools <unconfigured@null.spigotmc.org>
2016-11-29 10:55:43 -0800

    Colby to the rescue!

--- contactform/libs/index.php
+++ contactform/libs/index.php
@@ -1,45 +1,25 @@
 <?php
 
-if (isset($_POST["submit"])) {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $comments = $_POST['comments'];
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $comments = $_POST['comments'];
 
-  $from = 'Contact Form';
-  $to = 'lucastoltman@gmail.com';
-  $subject = 'Message from my Website!';
+    $to = 'lucastoltman@gmail.com';
+    $subject = 'Message from my Website!';
 
-  $body = "From: $name\n Email: $email\n Message:\n $comments";
+    $body = "From: $name\n Email: $email\n Message:\n $comments";
+
+    $headers = array(
+        'From: forge@padparadscha.voidteam.net',
+        'Reply-To: forge@padparadscha.voidteam.net',
+        'X-Mailer: PHP/' . PHP_VERSION
+    );
+
+    if (mail($to, $subject, $body, implode(' ', $headers))) {
+        echo '<div class="alert alert-success">Thanks I will be in touch!</div>';
+    } else {
+        echo '<div class="alert alert-danger">Sorry, an error occured. Your message failed to send.</div>';
+    }
 }
-
-/*if(isset($_POST['g-recaptcha-response'])){
-        $captcha=$_POST['g-recaptcha-response'];
-        }
-        if(!$captcha){
-          echo '<h2>Please check the captcha.</h2>';
-          exit;
-        }
-        $secretKey = "6Lc-RQ0UAAAAANpa5gh1Y38xCGWY6E9A4cuplByN";
-        $ip = $_SERVER['159.203.103.224'];
-        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
-        $responseKeys = json_decode($response,true);
-        if(intval($responseKeys["success"]) !== 1) {
-          echo '<h2>Thanks for the spam!</h2>';
-        } else {
-          echo '<h2>Thanks for the comment. I will get back to you as soon as I can.</h2>';
-        ]
-*/
-
-
-
-
-if (mail ($to, $subject, $body, $from)) {
-  echo '<div class="alert alert-success">Thanks I will be in touch!</div>';
-} else {
-  echo '<div class="alert alert-danger">Sorry, an error occured. Your message failed to send.</div>';
-}
-
-/* Redirect visitor to the thank you page */
-//header('Location: contact_thanks.html');
-
 ?>
