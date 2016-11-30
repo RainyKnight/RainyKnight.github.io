741fc497a2cd43e87d9cca5d93a0682d9bfb971c
Lucas <stoltmanlucas@gmail.com>
2016-11-30 13:57:02 -0800

    ReCaptcha implementation

--- contactform/libs/index.php
+++ contactform/libs/index.php
@@ -5,6 +5,7 @@ if (isset($_POST['submit'])) {
     $email = $name .'<trusted@email.com>';
     $actualemail = $_POST['email'];
     $comments = $_POST['comments'];
+    $captcha = $_POST['g-recaptcha-response'];
     
     $to = 'lucastoltman@gmail.com';
     
@@ -20,9 +21,20 @@ if (isset($_POST['submit'])) {
         'X-Mailer: PHP/' . PHP_VERSION
         
     );
-    if (mail($to, $subject, $body, implode(' ', $headers))) {
-        header('Location: http://lucasstoltman.com/contact_thanks.html');
-    } else {
-        echo '<div class="alert alert-danger">Sorry, an error occured. Your message failed to send.</div>';
-    }
+    
+    if(!$captcha){
+          echo '<h2>Please check the the captcha form.</h2>';
+          exit;
+        }
+        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6Lc-RQ0UAAAAANpa5gh1Y38xCGWY6E9A4cuplByN&response=".$captcha."&remoteip=".$_SERVER['159.203.103.224']);
+        if($response.success==false){
+          echo '<h2>Not on my watch!</h2>';
+        } else {
+            if (mail($to, $subject, $body, implode(' ', $headers))){
+                //header('Location: http://lucasstoltman.com/contact_thanks.html');
+            } else {
+                echo '<div class="alert alert-danger">Sorry, an error occured. Your message failed to send.</div>';
+            }
+          echo '<h2>Thanks for posting the lovely comment!</h2>';
+        }
 }
