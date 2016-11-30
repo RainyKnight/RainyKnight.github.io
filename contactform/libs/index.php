0b65fb9e7a00beb19344d9299651e8ef6966fe5d
BuildTools <unconfigured@null.spigotmc.org>
2016-11-29 19:20:23 -0800

    Added $email_to

--- contactform/libs/index.php
+++ contactform/libs/index.php
@@ -3,21 +3,25 @@
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
+    $email_to = "$name <$email>";
     $comments = $_POST['comments'];
 
     $to = 'lucastoltman@gmail.com';
     $subject = 'Message from my Website!';
 
-    $body = "From: $name\n Email: $email\n Message:\n $comments";
+
+    $body .= "From: $name\n Email: $email\n Message:\n $comments";
+
+
 
     $headers = array(
-        'From: forge@padparadscha.voidteam.net',
-        'Reply-To: forge@padparadscha.voidteam.net',
-        'X-Mailer: PHP/' . PHP_VERSION
+        'From:' . $email_to,
+        'Reply-To:' . $email,
+        //'X-Mailer: PHP/' . PHP_VERSION
     );
 
     if (mail($to, $subject, $body, implode(' ', $headers))) {
-        echo '<div class="alert alert-success">Thanks I will be in touch!</div>';
+        header('Location: http://lucasstoltman.com/contact_thanks.html');
     } else {
         echo '<div class="alert alert-danger">Sorry, an error occured. Your message failed to send.</div>';
     }
