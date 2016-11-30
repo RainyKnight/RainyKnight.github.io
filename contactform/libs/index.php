a63e0e769c531be6f8da7487496833bf3d4c6a67
Lucas <stoltmanlucas@gmail.com>
2016-11-30 14:18:31 -0800

    reCaptcha fully integrated! And email changed.

--- contactform/libs/index.php
+++ contactform/libs/index.php
@@ -7,7 +7,7 @@ if (isset($_POST['submit'])) {
     $comments = $_POST['comments'];
     $captcha = $_POST['g-recaptcha-response'];
     
-    $to = 'lucastoltman@gmail.com';
+    $to = 'stoltmanlucas@gmail.com';
     
     $subject = 'Message from: '. $actualname;
     
@@ -23,18 +23,18 @@ if (isset($_POST['submit'])) {
     );
     
     if(!$captcha){
-          echo '<h2>Please check the the captcha form.</h2>';
+          echo '<h2>Please check that you filled out the captcha form.</h2><br>
+                <p>You can click the back arrow without losing the information you entered. :)</p>';
           exit;
         }
-        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6Lc-RQ0UAAAAANpa5gh1Y38xCGWY6E9A4cuplByN&response=".$captcha."&remoteip=".$_SERVER['159.203.103.224']);
+        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6Lc-RQ0UAAAAANpa5gh1Y38xCGWY6E9A4cuplByN&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
         if($response.success==false){
           echo '<h2>Not on my watch!</h2>';
         } else {
             if (mail($to, $subject, $body, implode(' ', $headers))){
-                //header('Location: http://lucasstoltman.com/contact_thanks.html');
+                header('Location: http://lucasstoltman.com/contact_thanks.html');
             } else {
                 echo '<div class="alert alert-danger">Sorry, an error occured. Your message failed to send.</div>';
             }
-          echo '<h2>Thanks for posting the lovely comment!</h2>';
         }
 }
